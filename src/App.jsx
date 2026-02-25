4bb9d475f1a2ce8d204afc39fd4c0ef6f5d6806f
domanskey <domadam04@gmail.com>
2026-02-25 17:48:58 +0100

    feat: add skills section

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import { Header } from "./Header.jsx";
 import { About } from "./About.jsx";
 import { AfterHours } from "./Afterhours.jsx";
 import { Footer } from "./Footer.jsx";
+import { Skills } from "./Skills.jsx";
 
 export default function App() {
   return (
     <>
       <Header></Header>
       <About></About>
+      <Skills></Skills>
       <AfterHours></AfterHours>
       <Footer></Footer>
-      {/* <Effect></Effect> */}
+      <Effect></Effect>
     </>
   );
 }
 
-// function Effect() {
-//   return (
-//     <div className="absolute w-3/4 h-3/4 bottom-0 right-0 translate-x-1/3 translate-y-1/2 rounded-full blur-3xl opacity-15 bg-linear-to-r from-sky-500 to-indigo-500"></div>
-//   );
-// }
+function Effect() {
+  return (
+    <div className="absolute w-3/4 h-3/4 bottom-0 right-0 translate-x-1/3 translate-y-1/2 rounded-full blur-3xl opacity-15 bg-linear-to-r from-sky-500 to-indigo-500"></div>
+  );
+}
