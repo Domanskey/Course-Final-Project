8329eb7f645d77d830228529bdc26b0f53843d0e
domanskey <domadam04@gmail.com>
2026-03-02 18:39:12 +0100

    feat: remove effect

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,6 @@ export default function App() {
       <Projects></Projects>
       <AfterHours></AfterHours>
       <Footer></Footer>
-      {/* <Effect></Effect> */}
     </>
-  );
-}
-
-function Effect() {
-  return (
-    <div className="absolute w-3/4 h-3/4 bottom-0 right-0 translate-x-1/3 translate-y-1/2 rounded-full blur-3xl opacity-15 bg-linear-to-r from-sky-500 to-indigo-500"></div>
   );
 }
